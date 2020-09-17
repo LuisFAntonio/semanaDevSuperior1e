bbec130c7dc65a4e90b7ded57eec2b3eca8947d8
Luis Fillipi Antonio <luisguetz@gmail.com>
2020-09-17 17:16:13 -0300

    Criando página de listagem dos registros

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Records from './pages/Records';
